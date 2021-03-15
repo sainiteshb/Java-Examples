9f0e25ffb0546482e30372338ac2dc5c5c61884b
Sai Nitesh <67225871+sainiteshb@users.noreply.github.com>
2021-03-15 20:16:03 +0530

    Add files via upload

---
+++ ThreadDemo.java
@@ -0,0 +1,38 @@
+package first;
+
+public class ThreadDemo {
+	public static void main(String[] args) {
+
+		Thread t1 = new Thread(() -> {
+			for (int i = 0; i <= 5; i++) {
+				System.out.println("Hello");
+				try {
+					Thread.sleep(500);
+				} catch (Exception e) {}
+			}
+		});
+		Thread t2 = new Thread(() -> {
+			for (int i = 0; i <= 5; i++) {
+				System.out.println("Hi");
+				try {
+					Thread.sleep(500);
+				} catch (Exception e) {}
+			}
+		});
+		Thread t3 = new Thread(() -> {
+			for(int i =0; i<=5;i++)
+			{
+			   System.out.println("Ciao");
+			   try {
+				   Thread.sleep(500);
+				   
+			   }catch(Exception e) {
+				   
+			   }
+			}
+		});
+		t1.start();
+		t2.start();
+		t3.start();
+	}
+}
