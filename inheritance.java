bea7a5907dda0740fd4c1f64153f14e0feb82e85
Sai Nitesh <67225871+sainiteshb@users.noreply.github.com>
2021-03-15 20:15:32 +0530

    Add files via upload

---
+++ inheritance.java
@@ -0,0 +1,27 @@
+package first;
+
+class Calculator {
+	public int add(int i, int j) {
+		return i + j;
+	}
+	public int mul(int i, int j) {
+		return i*j;
+	}
+}
+
+class CalAdv extends Calculator {
+	public int sub(int i, int j) {
+		return i - j;
+	}
+}
+
+public class inheritance {
+
+	public static void main(String[] args) {
+		CalAdv c1 = new CalAdv();
+		int result1 = c1.add(5, 8);
+		int result2 = c1.mul(9, 4);
+		System.out.print(result1 +","+ result2);
+
+	}
+}
