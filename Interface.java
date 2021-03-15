db67572c00c57bb7b5a0d255be0197649f20d983
Sai Nitesh <67225871+sainiteshb@users.noreply.github.com>
2021-03-15 20:16:21 +0530

    Add files via upload

---
+++ Interface.java
@@ -0,0 +1,23 @@
+package first;
+
+interface Demo {
+	int num = 4;
+	void abc();
+	static void show() {
+		System.out.println("IN show");
+	}
+}
+class DemoImp implements Demo{
+	
+	public void abc() {
+		System.out.println("In abc");
+	}
+}
+
+public class Interface {
+	public static void main(String[] args) {
+		Demo obj = new DemoImp();
+		Demo.show();
+		obj.abc();
+	}
+}
