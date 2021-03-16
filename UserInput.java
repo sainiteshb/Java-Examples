387d273eb6bd83f3ebe853528fc014bff8faf23b
Sai Nitesh <67225871+sainiteshb@users.noreply.github.com>
2021-03-16 19:00:47 +0530

    Add files via upload

---
+++ UserInput.java
@@ -0,0 +1,14 @@
+package first;
+
+import java.util.Scanner;
+
+public class UserInput {
+	public static void main(String[] args) {
+		int n = 0;
+		System.out.println("Enter a number");
+		Scanner sc = new Scanner(System.in);
+
+		n = sc.nextInt();
+		System.out.println(n);
+	}
+}
