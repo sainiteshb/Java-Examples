0580cc29f99af3ba29da7a2814ec384116f99b43
Sai Nitesh <67225871+sainiteshb@users.noreply.github.com>
2021-03-16 18:59:13 +0530

    Add files via upload

---
+++ overriding.java
@@ -0,0 +1,30 @@
+package first;
+
+class A {
+	public void show() {
+		System.out.println("In A");
+	}
+}
+
+class B extends A {
+	public void show() {
+		System.out.println("In B");
+	}
+}
+
+class C extends A {
+	public void show() {
+		System.out.println("IN C");
+	}
+}
+
+public class overriding {
+	public static void main(String[] args) {
+		A obj = new A();
+		obj.show();
+
+		obj = new C();
+		obj.show();
+
+	}
+}
