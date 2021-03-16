2ad1bb649caa0150ecf1477ab12cb49959556ae4
Sai Nitesh <67225871+sainiteshb@users.noreply.github.com>
2021-03-16 18:56:53 +0530

    Add files via upload

---
+++ InnerDemo.java
@@ -0,0 +1,26 @@
+package first;
+
+class Outer {
+	int a;
+
+	public void show() {
+		System.out.println("In show");
+	}
+
+	static class Inner {
+		public void display() {
+			System.out.println("In display");
+		}
+	}
+}
+
+public class InnerDemo {
+
+	public static void main(String[] args) {
+		Outer obj = new Outer();
+		obj.show();
+
+		Outer.Inner obj1 = new Outer.Inner();
+		obj1.display();
+	}
+}
