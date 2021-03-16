7500b64ba7114b241e573ed0cc96b1a03566e957
Sai Nitesh <67225871+sainiteshb@users.noreply.github.com>
2021-03-16 18:57:27 +0530

    Add files via upload

---
+++ encapsultaion.java
@@ -0,0 +1,31 @@
+package first;
+
+class Student{
+	private int rollno;
+	private String name;
+	
+	public void setRollno(int r) {
+		this.rollno = r;
+	}
+	public int getRollno() {
+		return rollno;
+	}
+	public void setName(String n) {
+		this.name = n;
+	}
+	public String getName() {
+		return name;
+	}
+	
+}
+
+public class encapsultaion {
+   public static void main(String[] args) {
+	   Student s1 =new Student();
+	   s1.setRollno(7);
+	  System.out.print(s1.getRollno());
+	   
+   }
+   
+   
+}
