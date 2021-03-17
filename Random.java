d18f13593fc0d51266b0a1c6f6687c06d8e94990
Sai Nitesh <67225871+sainiteshb@users.noreply.github.com>
2021-03-17 18:55:13 +0530

    Add files via upload

---
+++ Random.java
@@ -0,0 +1,37 @@
+package first;
+
+public class Random {
+
+	public static void main(String args[]) {
+		
+		
+		int n = 5;
+		
+		switch(n) {
+		
+		case 1:
+			System.out.print("One");
+			break;
+		case 2:
+			System.out.print("Two");
+			break;
+		case 3:
+			System.out.print("Three");
+			break;
+		case 4:
+			System.out.print("Four");
+			break;
+		case 5:
+			System.out.print("Five");
+			break;
+		default:
+			System.out.print("Enter a valid number");
+			break;
+		}
+	   
+	
+		
+		
+	}
+	
+}
