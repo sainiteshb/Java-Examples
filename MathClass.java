47c71e0e6297c81123154c2a5a947697a0f3dfcd
Sai Nitesh <67225871+sainiteshb@users.noreply.github.com>
2021-04-15 20:45:47 +0530

    Add files via upload

---
+++ MathClass.java
@@ -0,0 +1,19 @@
+package first;
+
+public class MathClass {
+
+	public static void main(String[] args) {
+		
+		double x= 3.80;
+		double y = -10;
+		double z;
+		z = Math.max(x,y);
+		
+		z = Math.abs(y);
+		
+		z= Math.sqrt(x);
+		
+		z=Math.round(x);
+		System.out.println(z);
+	}
+}
