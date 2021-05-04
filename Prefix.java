c2f39ce3475176d8987cb71afa4418c501fd4394
Sai Nitesh <67225871+sainiteshb@users.noreply.github.com>
2021-05-04 19:58:12 +0530

    Add files via upload

---
+++ Prefix.java
@@ -0,0 +1,27 @@
+package first;
+
+public class Prefix {
+
+	static void fillPrefixSum(int arr[],int n,int prefixSum[]){
+		prefixSum[0] = arr[0];
+		for(int i =1;i< n;i++) {
+			prefixSum[i] = prefixSum[i-1] + arr[i];
+		}
+		
+	}
+	public static void main (String args[]) {
+		int arr[] = {10,20,30,40,50,60};
+		int n = arr.length;
+		int prefixSum[] = new int[n];
+		
+		fillPrefixSum(arr,n,prefixSum);
+		
+		for(int i=0;i<n;i++) {
+			System.out.print( prefixSum[i] +" ");
+			
+		}
+		
+		System.out.println();
+		
+	}
+}
